ca692e1c6921f8aa3eb3eb5952cd7db5def59425
rpizziol <roberto.pizziol@gmail.com>
2024-03-26 13:44:51 +0100

    Script to deploy the 3-tier application

---
+++ src/main/resources/deploy-3tier.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Tier 3
+kubectl apply -f tier3-deployment.yaml
+sleep 2
+kubectl apply -f tier3-service.yaml
+
+sleep 3
+
+# Tier 2
+kubectl apply -f tier2-deployment.yaml
+sleep 2
+kubectl apply -f tier2-service.yaml
+
+sleep 3
+
+# Tier 1
+kubectl apply -f tier1-deployment.yaml
+sleep 2
+kubectl apply -f tier1-service.yaml
