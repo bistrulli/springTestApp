edcdde82a12d36c878924164fe382479359e45ac
rpizziol <roberto.pizziol@gmail.com>
2024-04-05 11:41:31 +0200

    Update

--- src/main/resources/deploy-3tier.sh
+++ src/main/resources/deploy-3tier.sh
@@ -3,18 +3,18 @@
 # Tier 3
 kubectl apply -f tier3-deployment.yaml
 #sleep 2
-#kubectl apply -f tier3-service.yaml
+kubectl apply -f tier3-service.yaml
 
 sleep 3
 
 # Tier 2
 kubectl apply -f tier2-deployment.yaml
 #sleep 2
-#kubectl apply -f tier2-service.yaml
+kubectl apply -f tier2-service.yaml
 
 sleep 3
 
 # Tier 1
 kubectl apply -f tier1-deployment.yaml
 #sleep 2
-#kubectl apply -f tier1-service.yaml
+kubectl apply -f tier1-service.yaml
