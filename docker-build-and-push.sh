ca1ee4ed561d29b9f021f68a4bc7906f54db23a5
rpizziol <roberto.pizziol@gmail.com>
2024-04-22 11:18:08 +0200

    Deleted yaml files, docker build update

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="rpizziol/spring-test-app-tier1"
-TAG="0.20"
+IMAGE_NAME="rpizziol/spring-test-app-tier3"
+TAG="0.13"
 
 docker build --no-cache -t $IMAGE_NAME:$TAG . && docker push $IMAGE_NAME:$TAG
