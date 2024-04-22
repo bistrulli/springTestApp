2df9cfb0d00c9ee01500abf45337c6a6e740bc4e
rpizziol <roberto.pizziol@gmail.com>
2024-04-22 14:12:48 +0200

    Tier 2 fix

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="rpizziol/spring-test-app-tier1"
-TAG="0.22"
+IMAGE_NAME="rpizziol/spring-test-app-tier2"
+TAG="0.18"
 
 docker build --no-cache -t $IMAGE_NAME:$TAG . && docker push $IMAGE_NAME:$TAG
