161dd8b57eb1cdc64e84dd333c535238fe2c421e
rpizziol <roberto.pizziol@gmail.com>
2024-05-15 16:37:58 +0200

    Tier 1 new attempt for graceful stop

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME="rpizziol/spring-test-app-tier2"
+IMAGE_NAME="rpizziol/spring-test-app-tier1"
 TAG="0.27"
 
 docker build --no-cache -t $IMAGE_NAME:$TAG . && docker push $IMAGE_NAME:$TAG
